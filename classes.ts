6b45c123a70d0ddc25e3f16194459030f8e4d5a3
sandeep <sandeepndd777@gmail.com>
2025-04-15 15:39:37 +0530

    added phone number

--- classes.ts
+++ classes.ts
@@ -2,11 +2,13 @@ class Employee{
     name:String;
     age:number;
     department:String;
+    phoneNumber:number;
 
-    constructor(name:string,age:number,department:string){
+    constructor(name:string,age:number,department:string,phoneNumber:number){
         this.name=name;
         this.age=age;
-        this.department=department
+        this.department=department;
+        this.phoneNumber = phoneNumber;
          
         
     }
@@ -15,8 +17,8 @@ class Employee{
     }
 }
 
-const employee=new Employee("mahesh",30,"testing")
+const employee=new Employee("mahesh",30,"testing",987557)
 employee.employeeDetails()
 
-const anotherEmp=new Employee("suresh",32,"accounts")
+const anotherEmp=new Employee("suresh",32,"accounts",8658975)
 anotherEmp.employeeDetails()
